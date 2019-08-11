a8d74b46fb58c702926db4870dffc62dada9b388
tancona1 <trevorjancona@gmail.com>
2019-08-11 15:05:46 -0400

    Shop Update
    Devil May Cry style shop to buy new moves. Earn money from defeating enemies and completing levels. Slight changes to jump to allow for short hops.

---
+++ Source/BadBoy/UNav_Jump.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UNav_Jump.h"
+
+UNav_Jump::UNav_Jump()
+{
+}
+
+UNav_Jump::~UNav_Jump()
+{
+}
---
+++ Source/BadBoy/UNav_Jump.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class BADBOY_API UNav_Jump
+{
+public:
+	UNav_Jump();
+	~UNav_Jump();
+};
