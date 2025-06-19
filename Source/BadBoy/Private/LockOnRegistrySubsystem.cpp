d54d0d7c46a73163e422cae9fee384214a188a39
Trevor Ancona <trevorjancona@gmail.com>
2025-06-19 02:20:24 -0700

    Auto Camera Updates
    Camaera rotates with movement and attacks as well as panning with movement

---
+++ Source/BadBoy/Private/LockOnRegistrySubsystem.cpp
@@ -0,0 +1,178 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LockOnRegistrySubsystem.h"
+
+bool ULockOnRegistrySubsystem::addActorToRegistry(AActor* actor, bool priority)
+{
+	for (LockOnEntry* entry : LockOnActors)
+	{
+		//AActor* entryActor = entry->actor;
+		if (actor == entry->actor)
+		{
+			return false;
+		}
+	}
+
+	LockOnEntry* newEntry = new LockOnEntry();
+	newEntry->actor = actor;
+	newEntry->priority = priority;
+
+	LockOnActors.Add(newEntry);
+
+	return true;
+}
+
+bool ULockOnRegistrySubsystem::removeActorFromRegistry(AActor* actor)
+{
+	for (LockOnEntry* entry : LockOnActors)
+	{
+		//AActor* entryActor = entry->actor;
+		if (actor == entry->actor)
+		{
+			LockOnActors.Remove(entry);
+			
+			return true;
+		}
+	}
+	
+
+	return false;
+}
+
+bool ULockOnRegistrySubsystem::updateActorPriority(AActor* actor, bool priority)
+{
+	for (LockOnEntry* entry : LockOnActors)
+	{
+		//AActor* entryActor = entry->actor;
+		if (actor == entry->actor)
+		{
+			entry->priority = priority;
+
+			return true;
+		}
+	}
+
+
+	return false;
+}
+
+AActor* ULockOnRegistrySubsystem::getClosestTarget(AActor* StartActor)
+{
+	AActor* bestFit = nullptr;
+	float bestDistance = 10000000;
+
+	TArray<AActor*> viableTargets = getViableLockOnTargets(StartActor->GetActorLocation(), 10000);
+
+	for (AActor* actor : viableTargets)
+	{
+		double distance = FVector::Distance(StartActor->GetActorLocation(), actor->GetActorLocation());
+
+		if (distance < bestDistance)
+		{
+			bestDistance = distance;
+			bestFit = actor;
+		}
+	}
+
+	return bestFit;
+}
+
+AActor* ULockOnRegistrySubsystem::getNextTargetInDirection(AActor* StartActor, FVector SearchDirection)
+{
+	AActor* bestFit = nullptr;
+	float bestAngle = 360;
+
+	TArray<AActor*> viableTargets = getViableLockOnTargets(StartActor->GetActorLocation(), 10000);
+
+	for (AActor* actor : viableTargets)
+	{
+		FVector VectorToActor = (StartActor->GetActorLocation() - actor->GetActorLocation());
+		VectorToActor.Normalize();
+
+		float angle = getAngleDifferenceBetween2Vectors(VectorToActor, SearchDirection);
+
+		if (angle < bestAngle)
+		{
+			bestAngle = angle;
+			bestFit = actor;
+		}
+	}
+
+	return bestFit;
+}
+
+AActor* ULockOnRegistrySubsystem::getClosestTargetInDirection(AActor* StartActor, FVector SearchDirection, float maxDistance, float angleTolerance, float startOffset)
+{
+	AActor* bestFit = nullptr;
+	float bestDistance = 10000000;
+
+	FVector searchStart = StartActor->GetActorLocation() + (startOffset * SearchDirection);
+
+	TArray<AActor*> viableTargets = getViableLockOnTargets(StartActor->GetActorLocation(), maxDistance);
+
+	for (AActor* actor : viableTargets)
+	{
+		FVector VectorToActor = (actor->GetActorLocation() - searchStart);
+		VectorToActor.Normalize();
+
+		float angle = getAngleDifferenceBetween2Vectors(VectorToActor, SearchDirection);
+
+		if (angle <= angleTolerance)
+		{
+			double distance = FVector::Distance(searchStart, actor->GetActorLocation());
+
+			if (distance < bestDistance)
+			{
+				bestDistance = distance;
+				bestFit = actor;
+			}
+		}
+		
+	}
+
+	return bestFit;
+}
+
+TArray<AActor*> ULockOnRegistrySubsystem::getViableLockOnTargets(FVector searchStart, float maxDistance)
+{
+	TArray<AActor*> ViableTargets = TArray<AActor*>();
+
+
+	for (LockOnEntry* entry : LockOnActors)
+	{
+		if (entry->priority)
+		{
+			ViableTargets.Add(entry->actor);
+		}
+
+		else
+		{
+			AActor* candidate = entry->actor;
+
+			if (candidate->IsValidLowLevel())
+			{
+				double distance = FVector::Distance(searchStart, candidate->GetActorLocation());
+
+				if (distance <= maxDistance)
+				{
+					FHitResult result;
+					GetWorld()->LineTraceSingleByObjectType(result, searchStart, candidate->GetActorLocation(), FCollisionObjectQueryParams(ECC_TO_BITFIELD(ECC_WorldStatic) | ECC_TO_BITFIELD(ECC_WorldDynamic)));
+
+					//if there were no blocks in the line trace
+					if (!result.bBlockingHit)
+					{
+						ViableTargets.Add(candidate);
+					}
+				}
+			}
+		}
+	}
+
+	return ViableTargets;
+}
+
+float ULockOnRegistrySubsystem::getAngleDifferenceBetween2Vectors(FVector vector1, FVector vector2)
+{
+	return FMath::RadiansToDegrees(FMath::Acos(vector1 | vector2));
+}
---
+++ Source/BadBoy/Public/LockOnRegistrySubsystem.h
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "LockOnRegistrySubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BADBOY_API ULockOnRegistrySubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+
+
+public:
+
+	struct LockOnEntry {
+		bool priority;
+		AActor* actor;
+	};
+	TArray<LockOnEntry*> LockOnActors;
+
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Add Actor to Lock On Registry"), Category = "Lock On")
+	bool addActorToRegistry(AActor* actor, bool priority);
+	
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Remove Actor from Lock On Registry"), Category = "Lock On")
+	bool removeActorFromRegistry(AActor* actor);
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Update Actor Priority"), Category = "Lock On")
+	bool updateActorPriority(AActor* actor, bool priority);
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Get Closest LockOn Target"), Category = "Lock On")
+	AActor* getClosestTarget(AActor* StartActor);
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Get Next LockOn Target In Direction"), Category = "Lock On")
+	AActor* getNextTargetInDirection(AActor* StartActor, FVector SearchDirection);
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Get Closest LockOn Target In Direction"), Category = "Lock On")
+	AActor* getClosestTargetInDirection(AActor* StartActor, FVector SearchDirection, float maxDistance, float angleTolerance, float startOffset);
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "GetViable LockOn Targets"), Category = "Lock On")
+	TArray<AActor*> getViableLockOnTargets(FVector searchStart, float maxDistance);
+
+	float getAngleDifferenceBetween2Vectors(FVector vector1, FVector vector2);
+};
