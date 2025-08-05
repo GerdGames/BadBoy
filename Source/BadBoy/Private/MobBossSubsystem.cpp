efeec0117fb18ad7178ae8ba0da78fd9e8a4bb51
Trevor Ancona <trevorjancona@gmail.com>
2025-08-05 00:04:52 -0700

    Delayed Unlock
    Now Mob Boss will lock during cinematics, and have a delayed unlock afterward.

--- Source/BadBoy/Private/MobBossSubsystem.cpp
+++ Source/BadBoy/Private/MobBossSubsystem.cpp
@@ -23,9 +23,45 @@ bool UMobBossSubsystem::ReturnAttackToken()
 		attackTokenCount = maxAttackTokens;
 		return false;
 	}
-	else 
+	else
 	{
 		attackTokenCount++;
 		return true;
 	}
+}
+
+bool UMobBossSubsystem::LockTokens()
+{
+	if (locked == false)
+	{
+		attackTokenCount = 0;
+		locked = true;
+		return true;
+	}
+	//then mob boss must be already locked
+	else 
+	{
+		return false;
+	}
+}
+
+void UMobBossSubsystem::UnlockTokens()
+{
+	//return true to signal a successful unlock
+	if (locked == true)
+	{
+		attackTokenCount = maxAttackTokens;
+		locked = false;
+	}
+}
+
+void UMobBossSubsystem::DelayedUnlock(float lockTime)
+{
+	GetWorld()->GetTimerManager().SetTimer(UnlockDelayTimer, this, &UMobBossSubsystem::UnlockTokens, lockTime, true);
+}
+
+bool UMobBossSubsystem::SetMaxTokens(int newMaxTokens)
+{
+	maxAttackTokens = newMaxTokens;
+	return true;
 }
--- Source/BadBoy/Public/MobBossSubsystem.h
+++ Source/BadBoy/Public/MobBossSubsystem.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include <Kismet/GameplayStatics.h>
 #include "Subsystems/WorldSubsystem.h"
 #include "MobBossSubsystem.generated.h"
 
@@ -17,6 +18,9 @@ class BADBOY_API UMobBossSubsystem : public UWorldSubsystem
 private:
 	int attackTokenCount = 2;
 	int maxAttackTokens = 2;
+	bool locked = false;
+
+	FTimerHandle UnlockDelayTimer;
 
 public:
 	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Try To Take Attack Token"), Category = "Attack Token")
@@ -24,5 +28,17 @@ public:
 
 	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Return Attack Token"), Category = "Attack Token")
 	bool ReturnAttackToken();
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Lock Attack Tokens"), Category = "Attack Token")
+	bool LockTokens();
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Unlock Attack Tokens"), Category = "Attack Token")
+	void UnlockTokens();
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Delayed Unlock"), Category = "Attack Token")
+	void DelayedUnlock(float lockTime);
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Set Max Attack Tokens"), Category = "Attack Token")
+	bool SetMaxTokens(int newMaxTokens);
 	
 };
